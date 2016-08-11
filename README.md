39fb175ed12c90fa5e36d72a1c7707de6b83b4ad
禤锦良 <xuanjinliang@kugou.net>
2016-08-11 17:00:57 +0800

    添加新的插件

---
+++ README.md
@@ -0,0 +1,26 @@
+### 安装
+
+```javascript
+   npm install fis-postpackager-manifest
+```
+
+### 使用
+
+```javascript
+       
+    //fis3配置
+    .match('::package', {
+        postpackager: fis.plugin('manifest',{
+            imgAttr:['data-webporiginal','src'],	 //检测页面中img属性包括图片地址，默认src
+            webP:true,                           //开启记录webp格式,例如:***.png ***.png.webp
+            cacheFile: ['2016FXFestival'],       //开启要添加缓存的文件夹下的文件，或指定文件	默认全局缓存
+            ignoreFile: ['recruitArtist']        //过滤不缓存的文件夹下的所有文件，或指定文件 注:cacheFile开启后，不建议开启
+        })
+    })
+
+```
+
+此插件会查找所有的html文件，过滤出html中的css，script，img，内嵌css中的background-image的路径，并缓存生成 文件名+".appcache"的缓存文件，在html文件相同路径下，并且在html中动态插入appcache；
+
+(欢迎反馈BUG，方便提升插件的质量)
+
---
+++ index.js
@@ -0,0 +1,155 @@
+/**
+ * Created by timxuan on 2016/8/1.
+ */
+'use strict';
+
+let rStyleScript = /(?:\s*(<link([^>]*?)(stylesheet){1}([^>]*?)(?:\/)?>))/ig,
+    linkScript = /(?:(\s*<script([^>]*)>([\s\S]*?)<\/script>))/ig,
+    scriptSrc = /(?:\ssrc\s*=\s*)('([^']+)'|"([^\"]+)")/i,
+    styleUrl = /(?:\shref\s*=\s*)('([^']+)'|"([^"]+)"|[^\s\/>]+)/i,
+    imgReg = /<(img)\s+[\s\S]*?\/?>/ig,
+    bgReg = /url\(((?:'|")?[^;)]*(?:'|")?)\)/ig,
+    manifestCache = fis.project.getCachePath() + '/manifest-' + fis.project.currentMedia(),
+    manifestCacheJson = manifestCache + '/manifestData.json';
+
+module.exports = function(ret, conf, settings, opt){
+    let dirPath = fis.project.getProjectPath(),
+        imgAttr = settings.imgAttr || 'src',
+        webp = settings.webP || false,
+        cacheFile = settings.cacheFile || '',
+        ignoreFile = settings.ignoreFile || '';
+
+    if(imgAttr instanceof Array){
+        imgAttr = imgAttr.join("|");
+    }
+
+    if(cacheFile instanceof Array){
+        cacheFile = cacheFile.join("|");
+    }
+
+    if(ignoreFile instanceof Array){
+        ignoreFile = ignoreFile.join("|");
+    }
+
+    if(!fis.util.isDir(manifestCache)){
+        fis.util.mkdir(manifestCache);
+    }
+
+    if(!fis.util.isFile(manifestCacheJson)){
+        writeFile(manifestCacheJson,JSON.stringify({}));
+    }
+
+    let imgSrc = new RegExp("(?:\\\s(?:"+imgAttr+")\\\s*=\\\s*)('([^']+)'|\"([^\"]+)\"|[^'\"\\\s>]+)","i");
+    let manifestJson = readFile(manifestCacheJson),nullManifestJson = {};
+    manifestJson = JSON.parse(manifestJson);
+
+    fis.util.map(ret.src,function(subpath,file,i){
+        let array = [];
+        if(file.isHtmlLike && file.rExt == '.html'){
+
+            if(cacheFile && !file.origin.match(cacheFile)){
+                return;
+            }
+
+            if(ignoreFile && file.origin.match(ignoreFile)){
+                return;
+            }
+
+            let cacheTime = file.cache.timestamp,
+                ororiginFile = file.origin;
+
+            if(manifestJson[ororiginFile] && manifestJson[ororiginFile] == cacheTime){
+                nullManifestJson[ororiginFile] = cacheTime;
+                return;
+            }
+
+            let htmlPath = dirPath + file.release.replace(file.ext,''),
+                filename = file.filename,
+                Content = file.getContent();
+
+            array.push(file.release);
+
+            let linkArray = Content.match(rStyleScript),
+                scriptArray = Content.match(linkScript),
+                bgArray = Content.match(bgReg),
+                imgArray = Content.match(imgReg);
+
+            //css过滤
+            if(linkArray){
+                linkArray.forEach(function(v){
+                    let href = v.match(styleUrl);
+                    if(href){
+                        href = RegExp.$1.replace(/\'|\"/ig,'').trim();
+                        array.push(href);
+                    }
+                })
+            }
+
+            //js过滤
+            if(scriptArray){
+                scriptArray.forEach(function(v){
+                    let src = v.match(scriptSrc);
+                    if(src){
+                        src = RegExp.$1.replace(/\'|\"/ig,'').trim();
+                        array.push(src);
+                    }
+                })
+            }
+
+            //内嵌的css过滤
+            if(bgArray){
+                bgArray.forEach(function(v,i){
+                    let url = v.match(bgReg);
+                    if(url){
+                        url = RegExp.$1.replace(/\'|\"/ig,'').trim();
+                        array.push(url);
+                        if(webp){
+                            url += '.webp';
+                            array.push(url);
+                        }
+                    }
+                });
+            }
+
+            //img图片过滤
+            if(imgArray){
+                imgArray.forEach(function(v){
+                    let src = v.match(imgSrc);
+                    if(src && !RegExp.$1.match('data:') && RegExp.$1.match(/\.(?:jpg|png|jpeg|gif|webp)/)){
+                        src = RegExp.$1.replace(/\'|\"/ig,'').trim();
+                        array.push(src);
+                        if(webp){
+                            src += '.webp';
+                            array.push(src);
+                        }
+                    }
+                })
+            }
+
+            //创建appcache文件
+            let srcArray = ['CACHE MANIFEST','# Time: '+ new Date().getTime(),'CACHE:',array.join('\r\n'),"NETWORK:","*"],
+                appcacheName = htmlPath + '.appcache';
+            writeFile(appcacheName,srcArray.join('\r\n'));
+
+            Content = Content.replace(/<(html)[^>]*>/i,function(m,$1){
+                if($1){
+                    return m.replace($1,'html manifest="'+filename+'.appcache"');
+                }
+                return m;
+            });
+            file.setContent(Content);
+            nullManifestJson[ororiginFile] = cacheTime;
+        }
+    });
+    if(Object.keys(nullManifestJson).length){
+        writeFile(manifestCacheJson,JSON.stringify(nullManifestJson));
+    }
+};
+
+function writeFile(path,data){
+    fis.util.write(path, data, 'utf-8', false);
+}
+
+function readFile(path){
+    return fis.util.read(path,true);
+}
